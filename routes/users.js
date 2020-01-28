f0f91eaa11aca350d4d48fb2e4f289dbb512833e
Adam White <adamfrankwhite@gmail.com>
2020-01-28 09:32:26 +0000

    changed to bcrypt-js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 let User = require('../models/user.model')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-js')
 
 router.route('/').get((req, res) => {
     User.find()
