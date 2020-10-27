ecf11e698f182f337a14ed0b40ea761792586a28
Adam White <adamfrankwhite@gmail.com>
2020-10-27 16:26:15 +0000

    modified data type

--- models/beer.model.js
+++ models/beer.model.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const beerSchema = new Schema({
-    beerName: {type: String, required: true},
-    beerType: {type: String, required: true},
-    beerDescription: {type: String, required: true},
-    brewery: {type: String, required: true},
-    date: {type: Date, required: true},
-    stars: {type: String, required: false},
-    img: {type: String, required: true}
-}, {
-    timestamps: true
-})
+const beerSchema = new Schema(
+    {
+        beerName: { type: String, required: true },
+        beerType: { type: String, required: true },
+        beerDescription: { type: String, required: true },
+        brewery: { type: String, required: true },
+        date: { type: Date, required: true },
+        stars: { type: Number, required: false },
+        img: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-const Beer = mongoose.model('Beer', beerSchema)
+const Beer = mongoose.model("Beer", beerSchema);
 
-module.exports = Beer
+module.exports = Beer;
