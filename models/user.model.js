e888e9b27143b666bc334d373246d2b8f7ceab74
Adam White <adamfrankwhite@gmail.com>
2020-10-30 11:05:43 +0000

    add theme to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,58 +1,64 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt-nodejs')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 3,
+        },
+        password: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 5,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 6,
+        },
+        // NEW
+        beers: {
+            type: Array,
+            required: false,
+        },
+        theme: {
+            type: String,
+            required: false,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 5
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 6
-    },
-    // NEW
-    beers: {
-        type: Array,
-        required: false
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true
-})
+);
 
 userSchema.statics.authenticate = (username, password, callback) => {
-    User.findOne({username: username})
-        .exec( (error, user) => {
-            if (error) {
-                return callback(error)
-            } else if (!user) {
-                let error = new Error('User not found')
-                error.status = 401
-                return callback(error)
+    User.findOne({ username: username }).exec((error, user) => {
+        if (error) {
+            return callback(error);
+        } else if (!user) {
+            let error = new Error("User not found");
+            error.status = 401;
+            return callback(error);
+        }
+        bcrypt.compare(password, user.password, (error, result) => {
+            if (result === true) {
+                return callback(null, user);
+            } else {
+                return callback();
             }
-            bcrypt.compare(password, user.password, (error, result) => {
-                if (result===true) {
-                    return callback(null, user)
-                } else {
-                    return callback()
-                }
-            })
-        })
-}
+        });
+    });
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
